9799debdedd30940894750d857e6ce535e567e0c
元晨 <huanghanwen.hhw@alibaba-inc.com>
2023-06-04 12:31:15 +0800

    feat:listInstancesOfProject

--- client/api/console_admin_api.go
+++ client/api/console_admin_api.go
@@ -1234,6 +1234,86 @@ func (s *ConsoleAdminApiService) ListGames(
 	return varReturnValue, varHttpResponse, nil
 }
 
+// ListInstancesOfProject
+/*
+ * 分页获取项目中的实例
+ * @param varForms model.ConsoleAdminListInstancesOfProjectForms
+ */
+func (s *ConsoleAdminApiService) ListInstancesOfProject(
+    varForms *model.ConsoleAdminListInstancesOfProjectForms,
+) (model.ConsoleAdminListInstancesOfProjectResult, *http.Response, error) {
+	var (
+		varHttpMethod = strings.ToUpper("Post")
+        varReturnValue model.ConsoleAdminListInstancesOfProjectResult
+	)
+
+	// create path and map variables
+	varPath := s.client.cfg.Scheme + "://" + s.client.cfg.Host + "/consoleAdmin/listInstancesOfProject"
+
+	varHeaderParams := make(map[string]string)
+	varQueryParams := url.Values{}
+	varFormParams := url.Values{}
+
+	// to determine the Content-Type header
+	varHttpContentTypes := []string{"application/x-www-form-urlencoded"}
+
+	// set Content-Type header
+	varHttpContentType := selectHeaderContentType(varHttpContentTypes)
+	if varHttpContentType != "" {
+		varHeaderParams["Content-Type"] = varHttpContentType
+	}
+
+	// to determine the Accept header
+	varHttpHeaderAccepts := []string{"application/json"}
+
+	// set Accept header
+	varHttpHeaderAccept := selectHeaderAccept(varHttpHeaderAccepts)
+	if varHttpHeaderAccept != "" {
+		varHeaderParams["Accept"] = varHttpHeaderAccept
+	}
+	if varForms != nil && varForms.NextToken != nil {
+		varFormParams.Add("nextToken", parameterToString(*varForms.NextToken, ""))
+	}
+	if varForms != nil && varForms.MaxResult != nil {
+		varFormParams.Add("maxResult", parameterToString(*varForms.MaxResult, ""))
+	}
+	varFormParams.Add("projectId", parameterToString(varForms.ProjectId, ""))
+
+	r, err := s.client.prepareRequest(varPath, varHttpMethod, varHeaderParams, varQueryParams, varFormParams)
+	if err != nil {
+		return varReturnValue, nil, err
+	}
+
+	varHttpResponse, err := s.client.callAPI(r)
+	if err != nil || varHttpResponse == nil {
+		return varReturnValue, varHttpResponse, err
+	}
+
+    defer varHttpResponse.Body.Close()
+	varBody, err := ioutil.ReadAll(varHttpResponse.Body)
+	if err != nil {
+		return varReturnValue, varHttpResponse, err
+	}
+
+	if varHttpResponse.StatusCode < 300 {
+		// If we succeed, return the data, otherwise pass on to decode error.
+		err = s.client.decode(&varReturnValue, varBody, varHttpResponse.Header.Get("Content-Type"))
+		if err == nil { 
+			return varReturnValue, varHttpResponse, err
+		}
+	}
+
+	if varHttpResponse.StatusCode >= 300 {
+		newErr := GenericError{
+			body: varBody,
+			error: varHttpResponse.Status,
+		}
+		return varReturnValue, varHttpResponse, newErr
+	}
+
+	return varReturnValue, varHttpResponse, nil
+}
+
 // ListProjects
 /*
  * 分页获取项目列表
