14dfe0be2c70a088681f856940159079575e858b
元晨 <huanghanwen.hhw@alibaba-inc.com>
2023-08-10 19:03:13 +0800

    feat: getGameArchive

--- client/model/usercontollergetusergamearchiveresultmodel.go
+++ client/model/usercontollergetusergamearchiveresultmodel.go
@@ -11,7 +11,5 @@ type UsercontollerGetUserGameArchiveResultModel struct {
     DownloadUrl string `json:"downloadUrl,omitempty"`
         // 存档MD5
     ObjectMD5 string `json:"objectMD5,omitempty"`
-        // 存档ID
-    ArchiveId int64 `json:"archiveId,omitempty"`
-    Model UsercontollerGetUserGameArchiveResultModelModel `json:"model,omitempty"`
+    UserGameArchiveDTO UsercontollerGetUserGameArchiveResultModelUserGameArchiveDto `json:"userGameArchiveDTO,omitempty"`
 }
---
+++ client/model/usercontollergetusergamearchiveresultmodelusergamearchivedto.go
@@ -0,0 +1,14 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type UsercontollerGetUserGameArchiveResultModelUserGameArchiveDto struct {
+        // 游戏会话ID
+    GameSessionId string `json:"gameSessionId,omitempty"`
+        // 存档生成时间
+    GmtCreate int64 `json:"gmtCreate,omitempty"`
+}
