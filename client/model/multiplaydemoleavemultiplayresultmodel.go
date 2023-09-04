f4c842b58d9e6e518568660f9179b352c82db704
Iven <huanghanwen.hhw@alibaba-inc.com>
2023-09-04 10:46:28 +0800

    feat: update

---
+++ client/model/multiplaydemoleavemultiplayresultmodel.go
@@ -0,0 +1,11 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type MultiplayDemoLeaveMultiPlayResultModel struct {
+    DataVer int64 `json:"dataVer,omitempty"`
+}
