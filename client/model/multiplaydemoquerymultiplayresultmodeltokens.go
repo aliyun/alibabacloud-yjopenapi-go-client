dc3a0e9a8c525455769c1af96203632ce59aa8c0
Iven <huanghanwen.hhw@alibaba-inc.com>
2023-09-04 10:46:36 +0800

    feat: update

---
+++ client/model/multiplaydemoquerymultiplayresultmodeltokens.go
@@ -0,0 +1,16 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type MultiplayDemoQueryMultiPlayResultModelTokens struct {
+    AccountId string `json:"accountId,omitempty"`
+    TokenId string `json:"tokenId,omitempty"`
+    OfflineTs int64 `json:"offlineTs,omitempty"`
+    Session string `json:"session,omitempty"`
+    Admin bool `json:"admin,omitempty"`
+    ControlId int32 `json:"controlId,omitempty"`
+}
