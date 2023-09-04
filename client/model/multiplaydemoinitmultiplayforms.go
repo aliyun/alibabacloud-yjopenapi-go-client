cda7c568fc4d4404519dcd0dd1c155bc37616d6d
Iven <huanghanwen.hhw@alibaba-inc.com>
2023-09-04 10:46:18 +0800

    feat: update

---
+++ client/model/multiplaydemoinitmultiplayforms.go
@@ -0,0 +1,19 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type MultiplayDemoInitMultiPlayForms struct {
+    GameId *string `json:"gameId,omitempty"`
+    GameSession *string `json:"gameSession,omitempty"`
+    AppKey *string `json:"appKey,omitempty"`
+    Config *string `json:"config,omitempty"`
+    SchedulerConfig *string `json:"schedulerConfig,omitempty"`
+    Tokens *string `json:"tokens,omitempty"`
+    OpenAk *string `json:"openAk,omitempty"`
+    OpenSk *string `json:"openSk,omitempty"`
+    AccountId *string `json:"accountId,omitempty"`
+}
