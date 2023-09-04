aa7e728458b782d125b9ad0c93917b9ec1f0adb6
Iven <huanghanwen.hhw@alibaba-inc.com>
2023-09-04 14:57:28 +0800

    feat: update

---
+++ client/model/consoleadmingetgameversionresult.go
@@ -0,0 +1,14 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type ConsoleAdminGetGameVersionResult struct {
+    Success bool `json:"success,omitempty"`
+    Model ConsoleAdminGetGameVersionResultModel `json:"model,omitempty"`
+    MsgInfo string `json:"msgInfo,omitempty"`
+    MsgCode string `json:"msgCode,omitempty"`
+}
