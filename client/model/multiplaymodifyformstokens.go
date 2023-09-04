48f10b9b9662ab41b271321e7a303e4fde5f9542
Iven <huanghanwen.hhw@alibaba-inc.com>
2023-09-04 14:59:13 +0800

    feat: update

---
+++ client/model/multiplaymodifyformstokens.go
@@ -0,0 +1,12 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type MultiplayModifyFormsTokens struct {
+    TokenId string `json:"tokenId,omitempty"`
+    ControlId int32 `json:"controlId,omitempty"`
+}
