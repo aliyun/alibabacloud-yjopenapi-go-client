65a2aff123f9013ff33c3adb2827fc58d4c4f6c9
Iven <huanghanwen.hhw@alibaba-inc.com>
2023-11-01 12:34:49 +0800

    feat: update

---
+++ client/model/consoleadminlistactivateableinstancesresultmodel.go
@@ -0,0 +1,12 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type ConsoleAdminListActivateableInstancesResultModel struct {
+    DataList []ConsoleAdminListActivateableInstancesResultModelDataList `json:"dataList,omitempty"`
+    Count int64 `json:"count,omitempty"`
+}
