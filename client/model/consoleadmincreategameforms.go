b1c5ad0ac535ab029eeda184701f54653e32b5b5
Iven <huanghanwen.hhw@alibaba-inc.com>
2023-11-01 13:49:31 +0800

    feat: update

---
+++ client/model/consoleadmincreategameforms.go
@@ -0,0 +1,12 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type ConsoleAdminCreateGameForms struct {
+    GameName string `json:"gameName"`
+    PlatformType int64 `json:"platformType"`
+}
