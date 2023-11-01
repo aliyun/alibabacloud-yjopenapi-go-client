cd562f4651c8ddf6228a496fba40d1e97bdf530c
Iven <huanghanwen.hhw@alibaba-inc.com>
2023-11-01 13:22:16 +0800

    feat: update

---
+++ client/model/usercontrollergetgametrialsurplusdurationresultmodeltrialpolicyresponses.go
@@ -0,0 +1,14 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type UsercontrollerGetGameTrialSurplusDurationResultModelTrialPolicyResponses struct {
+        // 策略类型
+    PolicyType string `json:"policyType,omitempty"`
+        // 剩余试玩时长
+    SurplusDurationInSecond string `json:"surplusDurationInSecond,omitempty"`
+}
