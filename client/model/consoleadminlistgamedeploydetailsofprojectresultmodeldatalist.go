b80fab7e8ce42af0a5a800e4f2ed59fca95671fc
Iven <huanghanwen.hhw@alibaba-inc.com>
2024-01-04 00:22:28 +0800

    feat: update

---
+++ client/model/consoleadminlistgamedeploydetailsofprojectresultmodeldatalist.go
@@ -0,0 +1,14 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type ConsoleAdminListGameDeployDetailsOfProjectResultModelDataList struct {
+    VersionId string `json:"versionId,omitempty"`
+    AutoActiveStatus string `json:"autoActiveStatus,omitempty"`
+    VersionName string `json:"versionName,omitempty"`
+    DeployStatus string `json:"deployStatus,omitempty"`
+}
