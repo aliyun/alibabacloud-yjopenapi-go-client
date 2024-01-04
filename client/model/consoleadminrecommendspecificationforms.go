e4fd31e9eb07d3d4d9e63958faaeaddd7143c99e
Iven <huanghanwen.hhw@alibaba-inc.com>
2024-01-05 00:18:50 +0800

    feat: update

---
+++ client/model/consoleadminrecommendspecificationforms.go
@@ -0,0 +1,13 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type ConsoleAdminRecommendSpecificationForms struct {
+    GameId string `json:"gameId"`
+    GameVersionId string `json:"gameVersionId"`
+    PlatformType int32 `json:"platformType"`
+}
