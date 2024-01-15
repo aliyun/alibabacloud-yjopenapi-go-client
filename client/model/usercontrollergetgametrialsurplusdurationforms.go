98b5dfbbd6eeba819c455d0add3e7bd31b4513ef
Iven <huanghanwen.hhw@alibaba-inc.com>
2024-01-16 00:20:10 +0800

    feat: update

---
+++ client/model/usercontrollergetgametrialsurplusdurationforms.go
@@ -0,0 +1,14 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type UsercontrollerGetGameTrialSurplusDurationForms struct {
+        // 账号id
+    AccountId string `json:"accountId"`
+    GameId string `json:"gameId"`
+    ProjectId string `json:"projectId"`
+}
