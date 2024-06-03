952e4b770188d6f0c02326c6462cded5c3c58c05
Iven <huanghanwen.hhw@alibaba-inc.com>
2024-06-04 00:18:51 +0800

    feat: update

---
+++ client/model/consoleadminrttrendresultmodeldatadata.go
@@ -0,0 +1,12 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type ConsoleAdminRtTrendResultModelDataData struct {
+    TimeStamp int64 `json:"timeStamp,omitempty"`
+    Value string `json:"value,omitempty"`
+}
