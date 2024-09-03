2a5705c1eaac0c452c16cd819d388185814c2bea
Iven <huanghanwen.hhw@alibaba-inc.com>
2024-09-04 00:18:28 +0800

    feat: update

---
+++ client/model/usercontrollerdeletegamearchiveresultmodel.go
@@ -0,0 +1,11 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type UsercontrollerDeleteGameArchiveResultModel struct {
+    Status int32 `json:"status,omitempty"`
+}
