75c4e86168ebd43261fea6be2e58cca9c2b925a3
Iven <huanghanwen.hhw@alibaba-inc.com>
2024-09-27 00:21:50 +0800

    feat: update

---
+++ client/model/consoleadminsubmitrefundresultmodel.go
@@ -0,0 +1,14 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type ConsoleAdminSubmitRefundResultModel struct {
+        // refundOrderId
+    RefundOrderId string `json:"refundOrderId,omitempty"`
+        // 是否提交成功
+    SubmitSuccess bool `json:"submitSuccess,omitempty"`
+}
