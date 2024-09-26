4fbd455d5effe10ad7d5cfd77576af643a42e4f4
Iven <huanghanwen.hhw@alibaba-inc.com>
2024-09-27 00:23:54 +0800

    feat: update

---
+++ client/model/usercontrollergetusergamearchiveforms.go
@@ -0,0 +1,13 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type UsercontrollerGetUserGameArchiveForms struct {
+    AccountId string `json:"accountId"`
+    GameId string `json:"gameId"`
+    ProjectId string `json:"projectId"`
+}
