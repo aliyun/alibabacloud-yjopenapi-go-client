a7cc88796f5c68e626dd73ce8080c35fe829a5d3
Iven <huanghanwen.hhw@alibaba-inc.com>
2024-09-30 00:18:24 +0800

    feat: update

---
+++ client/model/aigetqueueresultmodel.go
@@ -0,0 +1,13 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type AiGetQueueResultModel struct {
+    QueueNum string `json:"queueNum,omitempty"`
+    Code string `json:"code,omitempty"`
+    RequestId string `json:"requestId,omitempty"`
+}
