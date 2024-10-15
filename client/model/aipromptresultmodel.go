e6d0643e303f8a464898a4df7937fe53dc910b12
Iven <huanghanwen.hhw@alibaba-inc.com>
2024-10-16 00:19:43 +0800

    feat: update

---
+++ client/model/aipromptresultmodel.go
@@ -0,0 +1,13 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type AiPromptResultModel struct {
+    RequestId string `json:"requestId,omitempty"`
+    TaskId string `json:"taskId,omitempty"`
+    Status string `json:"status,omitempty"`
+}
