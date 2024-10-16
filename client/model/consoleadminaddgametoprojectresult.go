f6151e003f39463d1d9997cba7e446a3f9f0e063
Iven <huanghanwen.hhw@alibaba-inc.com>
2024-10-17 00:20:24 +0800

    feat: update

---
+++ client/model/consoleadminaddgametoprojectresult.go
@@ -0,0 +1,13 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type ConsoleAdminAddGameToProjectResult struct {
+    Success bool `json:"success,omitempty"`
+    MsgInfo string `json:"msgInfo,omitempty"`
+    MsgCode string `json:"msgCode,omitempty"`
+}
