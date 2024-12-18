6379493d60c18c56ed059eae0a321f58046e06ce
Iven <huanghanwen.hhw@alibaba-inc.com>
2024-12-19 00:26:30 +0800

    feat: update

---
+++ client/model/consoleadmincreateorderresultmodel.go
@@ -0,0 +1,14 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type ConsoleAdminCreateOrderResultModel struct {
+        // 实例ID
+    InstanceId string `json:"instanceId,omitempty"`
+        // 订单ID
+    OrderId string `json:"orderId,omitempty"`
+}
