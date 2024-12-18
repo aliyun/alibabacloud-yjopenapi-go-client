d2fc6abc88a7f071b1f6e082271bdc7fba4dd0fe
Iven <huanghanwen.hhw@alibaba-inc.com>
2024-12-19 00:25:20 +0800

    feat: update

---
+++ client/model/aicreatenpcroleformsknowledgebases.go
@@ -0,0 +1,12 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type AiCreateNpcRoleFormsKnowledgeBases struct {
+    KnowledgeBaseVersionId int64 `json:"knowledgeBaseVersionId,omitempty"`
+    KnowledgeBaseId int64 `json:"knowledgeBaseId"`
+}
