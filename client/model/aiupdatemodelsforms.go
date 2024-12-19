fce1c3efa24113353d9fab0ba168646021d9586d
Iven <huanghanwen.hhw@alibaba-inc.com>
2024-12-20 00:18:45 +0800

    feat: update

---
+++ client/model/aiupdatemodelsforms.go
@@ -0,0 +1,12 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type AiUpdateModelsForms struct {
+    ResourceId string `json:"resourceId"`
+    Desc string `json:"desc"`
+}
