e759645f170bf2c99f30b506a4200054a1ad62fb
Iven <huanghanwen.hhw@alibaba-inc.com>
2024-12-20 00:19:49 +0800

    feat: update

---
+++ client/model/consoleadmindeleteprojectresult.go
@@ -0,0 +1,13 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type ConsoleAdminDeleteProjectResult struct {
+    Success bool `json:"success,omitempty"`
+    MsgInfo string `json:"msgInfo,omitempty"`
+    MsgCode string `json:"msgCode,omitempty"`
+}
