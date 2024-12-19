7220d8c8263edb961fd5646e627e16f35fb8a8b6
Iven <huanghanwen.hhw@alibaba-inc.com>
2024-12-20 00:24:42 +0800

    feat: update

---
+++ client/model/usercontrollerrestoregamearchiveresult.go
@@ -0,0 +1,14 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type UsercontrollerRestoreGameArchiveResult struct {
+    Success bool `json:"success,omitempty"`
+    Model UsercontrollerRestoreGameArchiveResultModel `json:"model,omitempty"`
+    MsgInfo string `json:"msgInfo,omitempty"`
+    MsgCode string `json:"msgCode,omitempty"`
+}
