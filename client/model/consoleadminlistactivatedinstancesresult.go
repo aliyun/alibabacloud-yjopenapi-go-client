131cdd3294847090972e63283566f2072cd38ea5
Iven <huanghanwen.hhw@alibaba-inc.com>
2025-01-16 00:22:44 +0800

    feat: update

---
+++ client/model/consoleadminlistactivatedinstancesresult.go
@@ -0,0 +1,14 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type ConsoleAdminListActivatedInstancesResult struct {
+    Success bool `json:"success,omitempty"`
+    Model ConsoleAdminListActivatedInstancesResultModel `json:"model,omitempty"`
+    MsgInfo string `json:"msgInfo,omitempty"`
+    MsgCode string `json:"msgCode,omitempty"`
+}
