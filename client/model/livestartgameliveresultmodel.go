27f30eef71914b025ea2a1d0044e41c37f1d9c73
Iven <huanghanwen.hhw@alibaba-inc.com>
2025-01-18 00:25:11 +0800

    feat: update

---
+++ client/model/livestartgameliveresultmodel.go
@@ -0,0 +1,13 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type LiveStartGameLiveResultModel struct {
+    Data bool `json:"data,omitempty"`
+    LiveId string `json:"liveId,omitempty"`
+    Status string `json:"status,omitempty"`
+}
