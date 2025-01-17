96c995a7510aa7e5668b857de69f4a4597c3cb6f
Iven <huanghanwen.hhw@alibaba-inc.com>
2025-01-18 00:25:17 +0800

    feat: update

---
+++ client/model/multiplaycloseforms.go
@@ -0,0 +1,12 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type MultiplayCloseForms struct {
+    MpId string `json:"mpId"`
+    Reason *string `json:"reason,omitempty"`
+}
