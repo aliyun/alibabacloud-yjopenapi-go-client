cd09af2e0777293312b5f5ea951eaa43c4550a0a
Iven <huanghanwen.hhw@alibaba-inc.com>
2025-03-11 00:17:21 +0800

    feat: update

---
+++ client/model/consoleadminremovegamefromprojectforms.go
@@ -0,0 +1,12 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type ConsoleAdminRemoveGameFromProjectForms struct {
+    ProjectId string `json:"projectId"`
+    GameId string `json:"gameId"`
+}
