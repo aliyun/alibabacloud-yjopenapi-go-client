52a16c72619ccae4e879aa3f53023c93867114eb
Iven <huanghanwen.hhw@alibaba-inc.com>
2025-03-13 00:29:00 +0800

    feat: update

---
+++ client/model/consoleadminqueryadaptresultbyversionidforms.go
@@ -0,0 +1,11 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type ConsoleAdminQueryAdaptResultByVersionIdForms struct {
+    MixVersionId string `json:"mixVersionId"`
+}
