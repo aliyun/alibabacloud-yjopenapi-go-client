8170d9bd99e0d43ead6ff408cc3bdf14b8dffd5f
Iven <huanghanwen.hhw@alibaba-inc.com>
2025-03-13 00:32:06 +0800

    feat: update

---
+++ client/model/usercontrollerupdategamearchivetagstatusforms.go
@@ -0,0 +1,14 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type UsercontrollerUpdateGameArchiveTagStatusForms struct {
+    AccountId string `json:"accountId"`
+    GameId string `json:"gameId"`
+    ArchiveId string `json:"archiveId"`
+    TagStatus int32 `json:"tagStatus"`
+}
