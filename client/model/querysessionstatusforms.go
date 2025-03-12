672bcb9fd5ff249c1ac9dd47fad174636dc03638
Iven <huanghanwen.hhw@alibaba-inc.com>
2025-03-13 00:30:55 +0800

    feat: update

---
+++ client/model/querysessionstatusforms.go
@@ -0,0 +1,14 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type QuerySessionStatusForms struct {
+        // 会话ID
+    GameSession string `json:"gameSession"`
+        // 项目应用AK
+    AppKey string `json:"appKey"`
+}
