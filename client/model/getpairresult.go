8435e6a7bf6d725efa76b17b346d99d68dedb880
Iven <huanghanwen.hhw@alibaba-inc.com>
2025-03-13 00:29:45 +0800

    feat: update

---
+++ client/model/getpairresult.go
@@ -0,0 +1,20 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type GetPairResult struct {
+        // token失效时间戳,单位:秒
+    Expired string `json:"expired,omitempty"`
+        // 返回码
+    Code string `json:"code,omitempty"`
+        // 返回信息
+    Message string `json:"message,omitempty"`
+        // 临时token
+    Token string `json:"token,omitempty"`
+        // 临时secretKey
+    AccessSecret string `json:"accessSecret,omitempty"`
+}
