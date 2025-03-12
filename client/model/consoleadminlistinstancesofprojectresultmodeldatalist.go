776ea54365769d7ee6e8d6a2fe914c0cb681f7b8
Iven <huanghanwen.hhw@alibaba-inc.com>
2025-03-13 00:28:42 +0800

    feat: update

---
+++ client/model/consoleadminlistinstancesofprojectresultmodeldatalist.go
@@ -0,0 +1,25 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type ConsoleAdminListInstancesOfProjectResultModelDataList struct {
+    CommoditySpecificationCnName string `json:"commoditySpecificationCnName,omitempty"`
+    BandwidthChargeType string `json:"bandwidthChargeType,omitempty"`
+    EffectiveTime int32 `json:"effectiveTime,omitempty"`
+    PlatformType string `json:"platformType,omitempty"`
+    PaymentType string `json:"paymentType,omitempty"`
+    Token string `json:"token,omitempty"`
+    ExpireTime int32 `json:"expireTime,omitempty"`
+    CommodityInstanceName string `json:"commodityInstanceName,omitempty"`
+    CommodityInstanceId string `json:"commodityInstanceId,omitempty"`
+    Region string `json:"region,omitempty"`
+    ProjectName string `json:"projectName,omitempty"`
+    ProjectId string `json:"projectId,omitempty"`
+    ContainerCount string `json:"containerCount,omitempty"`
+    Status string `json:"status,omitempty"`
+    TradeChannel string `json:"tradeChannel,omitempty"`
+}
