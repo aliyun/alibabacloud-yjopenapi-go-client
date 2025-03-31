5a1ec504c05b72405554b55cf28e2f958c817e0e
Iven <huanghanwen.hhw@alibaba-inc.com>
2025-04-01 00:12:55 +0800

    feat: update

---
+++ client/model/ailistmodelsresultmodelmodellistitems.go
@@ -0,0 +1,16 @@
+// Package client
+/*
+ * YuanJing OpenAPI SDK for Go
+ *
+ *
+ */
+package model
+
+type AiListModelsResultModelModelListItems struct {
+    ModelSeries string `json:"modelSeries,omitempty"`
+    ModelType string `json:"modelType,omitempty"`
+    ModelFileType string `json:"modelFileType,omitempty"`
+    ModelFileName string `json:"modelFileName,omitempty"`
+    Url string `json:"url,omitempty"`
+    Desc string `json:"desc,omitempty"`
+}
